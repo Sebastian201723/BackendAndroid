e9d03a8634b994ce2f4625521faf7150358e30fd
Sebastian Cajas <ulsordonez@unicauca.edu.co>
2017-10-21 14:48:25 -0500

    Ultimo

--- routes/reservas.js
+++ routes/reservas.js
@@ -18,7 +18,7 @@ router.get("/", (req, res, next) => {
 
 router.get("/:_idusuario", (req, res, next) => {
     let id = req.params._idusuario;
-    req.collection.find({ _idusuario: id }).toArray()
+    req.collection.find({ id: _idusuario }).toArray()
         .then(result => {
             if (result) {
                 res.send(result);
